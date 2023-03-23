46f3094e4d2c7e9174c4520a0f5699cc9c6158e5
Fernando Jimenez <fgjv06@gmail.com>
2023-03-23 07:38:03 -0300

    JS - Calling playTune() function with passing data-key value as an argument

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
 pianoKeys.forEach(key => {
+  // Calling playTune function with passing data-key value as an argument
+  key.addEventListener("click", () => playTune(key.dataset.key));
   console.log(key.dataset.key);
 })

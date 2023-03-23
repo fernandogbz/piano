030a7c7d9fd1ebe265e45f8c4d232ff84810573e
Fernando Jimenez <fgjv06@gmail.com>
2023-03-23 08:47:24 -0300

    JS - keysCheckbox created

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key"),
-volumeSlider = document.querySelector(".volume-slider input");
+volumeSlider = document.querySelector(".volume-slider input"),
+keysCheckbox = document.querySelector(".keys-checkbox input");
 
 let allKeys = [],
 audio = new Audio("tunes/a.wav"); // by default, audio src is "a" tune

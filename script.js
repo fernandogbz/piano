38b7d11243ae25f140604ea6caa6cf73dc907fbc
Fernando Jimenez <fgjv06@gmail.com>
2023-03-23 07:47:26 -0300

    JS - creating a new HTML audio element

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let audio = new Audio("tunes/a.wav"); // by default, audio src is "a" tune
+
+
 pianoKeys.forEach(key => {
   // Calling playTune function with passing data-key value as an argument
   key.addEventListener("click", () => playTune(key.dataset.key));
-  console.log(key.dataset.key);
 })
